b23842536bafd0439cf6cabef42f9655defe562c
Andrew <andrew.krohmalyuk12@gmail.com>
2017-08-26 02:47:22 +0300

    some fixed

--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,17 @@
+var run = false;
 
 function setWindowView() {
     if($(window).width() < 900) {
-
+        if(run){
+            $.fn.fullpage.destroy('all');
+        }
+        run = false;///Мое решение
     } else {
-        $('.fullpage').fullpage({
-            controlArrows: false,
-        });
+        if(!run) {
+            $('.fullpage').fullpage({
+                controlArrows: false,
+            });
+            run = true;
+        }
     }
 }
