91b438536618aa4f09a094e1d365a805d1c93128
Andrew <andrew.krohmalyuk12@gmail.com>
2017-08-29 19:44:57 +0300

    Added nav btn

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 $(function(){
+    var count = 1;
+
     setWindowView();
     $(window).resize(function(){
         setWindowView();
@@ -19,4 +21,20 @@ $(function(){
         var target = $(this).attr('data-index');
         _scrollTo(target);
     });
+
+    $('.nav-btn .btn').click(function(){
+        // _scrollTo(count);
+        if(!$(this).hasClass('up')) {
+            count++;
+            if(count > 4) count = 4;
+            _scrollTo(count);
+            console.log(count);
+        } else {
+            count--;
+            if(count < 1) count = 1;
+            _scrollTo(count);
+            console.log(count);
+        }
+    })
+
 });
