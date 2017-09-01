56e97100d4fb9e5414cde0995c4bb559b0dd97af
Andrew <andrew.krohmalyuk12@gmail.com>
2017-09-01 12:21:53 +0300

    Added scroll event

--- js/main.js
+++ js/main.js
@@ -24,21 +24,27 @@ $(function(){
             _scrollTo(target);
         } else {
             currSlide = target - 1;
-            tabsController(currSlide);
+            tabsController(curr);
             $.fn.fullpage.moveTo('section', currSlide);
         }
     });
     $('.nav-btn .btn').click(function(){
         if(!$(this).hasClass('up')) {
             count++;
-            if(count > 4) count = 4;
-            _scrollTo(count);
-            console.log(count);
+            if(count > 4) {
+                count = 4;
+            } else {
+                _scrollTo(count);
+                //console.log(count);
+            }
         } else {
             count--;
-            if(count < 1) count = 1;
-            _scrollTo(count);
-            console.log(count);
+            if(count < 1) {
+                count = 1;
+            } else {
+                _scrollTo(count);
+                //console.log(count);
+            }
         }
     })
     $('.slide-btn .btn').click(function(){
@@ -61,5 +67,22 @@ $(function(){
         $(this).addClass('active');
         currSlide = parseFloat($(this).attr('data-index') - 1);
         $.fn.fullpage.moveTo('section', currSlide);
+    });
+    $(window).scroll(function() {
+        //console.log(window.pageYOffset);
+        if(window.pageYOffset > 90 && window.pageYOffset < 740) {
+            count = 1;
+            //console.log(count);
+        } else if (window.pageYOffset > 740 && window.pageYOffset < 1400) {
+            count = 2;
+            //console.log(count);
+        } else if (window.pageYOffset > 1400 && window.pageYOffset < 2060) {
+            count = 3;
+            //console.log(count);
+        }
+        else if (window.pageYOffset > 2060) {
+            count = 4;
+            //console.log(count);
+        }
     });
 });
